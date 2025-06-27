1445b876ec7012ec58bb0ccb8d9830395cf0ec69
Hemlata Sharma <hemlatasharma23052003@gmail.com>
2025-06-27 12:46:41 +0530

    structuring the data

---
+++ preprocessor.py
@@ -0,0 +1,56 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    # Updated regex: allow for narrow non-breaking space before am/pm
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}[\u202f ]?[apAP][mM]\s-\s'
+
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    # Remove weird unicode characters from date strings
+    clean_dates = [re.sub(r'[\u202f\u200e]', '', date) for date in dates]
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': clean_dates})
+
+    # Updated datetime format (2-digit year, 12-hour time, no seconds)
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M%p - ', errors='coerce')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    # Create new time-related features
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    # Hour buckets
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append(f"{hour}-00")
+        elif hour == 0:
+            period.append("00-1")
+        else:
+            period.append(f"{hour}-{hour+1}")
+    df['period'] = period
+
+    return df
